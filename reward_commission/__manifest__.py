b32582a4a79a7f4ca26a8c22aae49f77bf4a2e67
Blouk <nicolas@blouk.com>
2020-06-16 11:23:55 +0200

    [FIRST] first commit

---
+++ reward_commission/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name' : 'Invoicing',
+    'version' : '1.1',
+    'summary': 'Reward to resellers and hairdressers by commisions',
+    'category': 'Tools',
+    'depends' : [],
+    'data': [    ],
+
+    'qweb': [
+
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
